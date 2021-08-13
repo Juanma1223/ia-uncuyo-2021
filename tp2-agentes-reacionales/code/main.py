e5bc414a23761b1a69814204fdf2a46cca6637e4
Juanma Fernandez <juanmanuelf12@gmail.com>
2021-08-13 19:02:49 -0300

    added random agent

--- tp2-agentes-reacionales/code/main.py
+++ tp2-agentes-reacionales/code/main.py
@@ -1,21 +1,47 @@
 from environment import Environment
 from agent import Agent
+from random_agent import RandomAgent
 
 #x = int(input("Tamaño:"))
-x = 128
+#rate = float(input("Ratio suciedad:"))
+
+#El entorno puede o no ser cuadrado, 
+x = 8
 print("x:"+ str(x))
-rate = float(input("Ratio suciedad:"))
+#Ratio de aparición de suciedad
+rate = 0.6
 
+#El entorno puede, o no, ser cuadrado
+#Inicialización de los entornos
 env = Environment(x,x,rate)
-agent = Agent(env,1000)
+env2 = Environment(x,x,rate)
 
+#Inicialización del agente aspiradora
+agent = Agent(env,1)
+
+#Inicialización del agente aleatorio
+rand_agent = RandomAgent(env2,10000)
+
+#Mostramos el entorno en el que está el agente aspiradora
 print("Entorno al inicio")
 env.print_environment()
-#Paso del tiempo
+
+#Simulación del tiempo en el agente aspiradora
 while(agent.currLife > 0):
     agent.think()
 
+#Simulación del agente aleatorio
+while(rand_agent.currLife > 0):
+    rand_agent.think()
+
+#Se muestra el entorno luego de la vida útil del agente aspiradora
 print("Entorno final")
 env.print_environment()
+
+#Se calcula el porcentaje de suciedad en el ambiente
 print(env.get_performance())
+#Se calcula el performance del agente aspiradora
 print(agent.get_performance())
+
+#Se calcula la performance del agente aleatorio
+print("Performance del agente aleatorio:",rand_agent.get_performance())
---
+++ tp2-agentes-reacionales/code/random_agent.py
@@ -0,0 +1,79 @@
+from random import randint
+
+class RandomAgent:
+    currLife = 0
+    totalLife = 0
+    currCleaned = 0
+    def __init__(self,env,totalLife):
+        #Inicializamos el entorno donde se encuentra el agente
+        self.__env = env
+        #Inicializamos la cantidad de acciones en 1000
+        self.currLife = totalLife
+        self.totalLife = totalLife
+        #Inicializamos la posicion de manera aleatoria
+        self.__currX = randint(0, env.get_sizeX())
+        self.__currY = randint(0, env.get_sizeY())
+
+    def perspective(self): #sensa el entorno
+        
+        if(floorSituation == 1):
+            self.clean()
+            #Usamos una accion
+        else:
+            self.idle()
+
+    def clean(self):
+        # Limpia
+        self.__env.set_clean(self.__currX,self.__currY)
+        #Revisamos si debemos sumar en performance
+        floorSituation = self.__env.is_dirty(self.__currX,self.__currY)
+        if (floorSituation == True):
+            self.currCleaned += 1
+
+    def up(self):
+        try:
+            self.__currY += 1
+        except:
+            print("",end="")
+
+    def down(self):
+        try:
+            self.__currY -= 1
+        except:
+            print("",end="")
+
+    def left(self):
+        try:
+            self.__currX -= 1
+        except:
+            print("",end="")
+
+    def right(self):
+        try:
+            self.__currX += 1
+        except:
+            print("",end="")
+
+    def idle(self):
+        print("",end="") # no hace nada
+
+    def get_performance(self):
+        return "Cuadriculas limpiadas: " + str(self.currCleaned)
+    
+    def think(self): # implementa las acciones a seguir por el agente
+        #Ejecuta aleatoriamente una acción
+        desition = randint(0,5)
+        if(desition == 0):
+            self.up()
+        elif(desition == 1):
+            self.right()
+        elif(desition == 2):
+            self.down()
+        elif(desition == 3):
+            self.left()
+        elif(desition == 4):
+            self.idle()
+        else:
+            self.clean()
+        self.currLife -= 1
+        
